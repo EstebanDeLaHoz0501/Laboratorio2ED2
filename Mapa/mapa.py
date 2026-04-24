e0ef459114fb4c3076475bf4647bd68182ec03e5
EstebanDeLaHoz0501 <estebandelahoz@uninorte.edu.co>
2026-04-23 22:05:44 -0500

    Menu

--- Mapa/mapa.py
+++ Mapa/mapa.py
@@ -2,16 +2,21 @@ import folium
 from modelos.lista_info_aeropuertos import cargar_datos
 from modelos.lista_adj_aeropuertos import lista_adyacencia
 from controlador.Operaciones_Grafo import operaciones_grafo
+import webbrowser
 
 aereopuertos = cargar_datos("flights/flights_final.csv")
 adyacencia = lista_adyacencia("flights/flights_final.csv")
 Opgrafo = operaciones_grafo("flights/flights_final.csv")
 
 cambioParaGrupo = 0
-mapa = folium.Map(location=[0, 0], zoom_start=2)
+
 
 def punto5(Ori, Des):
-    cambioParaGrupo =+1
+    if Ori not in adyacencia:
+        return False
+    if Des not in adyacencia:
+        return False
+    mapa = folium.Map(location=[0, 0], zoom_start=2)
     distancia, camino, caminoNodos = Opgrafo.caminoEntreNodos(Ori, Des)
     grupo = folium.FeatureGroup(name=cambioParaGrupo)
     for nodo in caminoNodos:
@@ -21,7 +26,18 @@ def punto5(Ori, Des):
         ).add_to(grupo)
 
     grupo.add_to(mapa)
-    mapa.save("mapa.html")
-
     folium.PolyLine(camino, tooltip=distancia).add_to(mapa)
     mapa.save("mapa.html")
+    webbrowser.open("mapa.html")
+    return True
+
+def punto6():
+    mapa = folium.Map(location=[0, 0], zoom_start=2)
+    for nodo in adyacencia:
+        folium.Marker(
+            location=[aereopuertos[nodo].lat, aereopuertos[nodo].lon],
+            popup=aereopuertos[nodo]
+        ).add_to(mapa)
+
+    mapa.save("mapa.html")
+    webbrowser.open("mapa.html")
